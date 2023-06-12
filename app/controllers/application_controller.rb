74037281d1a3f7eb4f9210ff7f7bc2d23c0d8262
Sergei O. Udalov <udalov.x@mail.ru>
2023-06-12 12:47:29 +0300

    user locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
   after_action :verify_authorized, unless: -> { devise_controller? }
+
+  around_action :switch_locale
+
+  def switch_locale(&)
+    if signed_in?
+      locale = current_user.locale || I18n.default_locale
+      I18n.with_locale(locale, &)
+    else
+      I18n.with_locale(I18n.default_locale, &)
+    end
+  end
 end
---
+++ db/migrate/20230612092503_add_user_locale.rb
@@ -0,0 +1,7 @@
+class AddUserLocale < ActiveRecord::Migration[7.0]
+  def change
+    change_table(:users) do |t|
+      t.column :locale, :string, limit: 3
+    end
+  end
+end
