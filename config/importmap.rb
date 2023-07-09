90b680484002843ee1d34d9515b500ba19e6a34a
Sergei O. Udalov <udalov.x@mail.ru>
2023-07-09 16:57:21 +0300

    build fixed

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin "echarts", to: "echarts.min.js"
-pin "echarts/theme/dark", to: "echarts/theme/dark.js"
+pin 'echarts', to: 'echarts.min.js'
+pin 'echarts/theme/dark', to: 'echarts/theme/dark.js'
